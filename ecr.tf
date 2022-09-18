79ef605b30c67428be25e0fe6a47260248aed09f
Khalid Shaikh <k@ruby.run>
2022-09-18 00:42:56 -0500

    u

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "ecr" {
   name                  = "nginx"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability  = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
